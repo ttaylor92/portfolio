63253d764908a4e871a98dcaee5d5d9b5bd7091f
Tyrone <tyronetaylorbiz@gmail.com>
2019-05-17 21:18:31 -0500

    Updates

--- precache-manifest.a6341ee20f88212c6ee7de065fcc5814.js
+++ precache-manifest.a6341ee20f88212c6ee7de065fcc5814.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "02bd35a2f39ffd6f0fa1dc1c7c3ea165",
+    "revision": "3ec33905ea9f367cc8864ffe4d6c45d9",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.c4c10d47.chunk.css"
   },
   {
-    "revision": "cc676bc64766e8235233",
-    "url": "/portfolio/static/css/main.89b25205.chunk.css"
+    "revision": "064a67bacceaf68bb586",
+    "url": "/portfolio/static/css/main.3dd12505.chunk.css"
   },
   {
     "revision": "52c84193f68a51e83fed",
     "url": "/portfolio/static/js/2.d9dd7d59.chunk.js"
   },
   {
-    "revision": "cc676bc64766e8235233",
-    "url": "/portfolio/static/js/main.07681322.chunk.js"
+    "revision": "064a67bacceaf68bb586",
+    "url": "/portfolio/static/js/main.56aa9c87.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6eb62385febaea830ab560d23cad06e6.js"
+  "/portfolio/precache-manifest.a6341ee20f88212c6ee7de065fcc5814.js"
 );
 
 self.addEventListener('message', (event) => {
